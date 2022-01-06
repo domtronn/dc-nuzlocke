a3b3fba8d6105542a567089d08fbaa7f21ce70ed
domtronn <dgc336@gmail.com>
2022-01-06 08:09:35 +0000

    [poc] More native mobile style nav

--- src/lib/components/navs/GameHeading.svelte
+++ src/lib/components/navs/GameHeading.svelte
@@ -41,55 +41,58 @@
   <div class=p-container>
     <div class='inline-flex items-center'>
       <a
-      href="/"
-      rel="external"
-      class='{className} home group'>
-      {#if game?.game}
-        <Picture
-          src=/assets/{game?.game}
-          className='h-7 w-auto my-1 md:mr-4 md:w-20 md:h-auto'
-          alt='{game?.game} logo'
-          aspect=192x96
-        />
-        <h1 in:fade class='hidden md:block group-hover:border-black dark:group-hover:border-white'>
-          {game?.name || ''}
-        </h1>
-      {/if}
-    </a>
+        href="/"
+        rel="external"
+        class='{className} home group'>
+        {#if game?.game}
+          <Picture
+            src=/assets/{game?.game}
+            className='h-7 w-auto my-1 md:mr-4 md:w-20 md:h-auto'
+            alt='{game?.game} logo'
+            aspect=192x96
+            />
+          <h1 in:fade class='hidden md:block group-hover:border-black dark:group-hover:border-white'>
+            {game?.name || ''}
+          </h1>
+        {/if}
+      </a>
 
       {#if games.length}
         <Popover className='mt-1 sm:mt-4' title='Load saves' position={window?.innerWidth < 700 ? 'bottom' : 'right'}>
-          <span class='inline-flex'>
-            <Icon className='transition fill-current ml-2' src={Save} />
-            <Icon className='hidden sm:block transition fill-current -ml-0.5' src={RightArr} />
-            <Icon className='block sm:hidden transition fill-current -ml-0.5' src={DownArr} />
-          </span>
+                 <span class='inline-flex'>
+                   <Icon className='transition fill-current ml-2' src={Save} />
+                   <Icon className='hidden sm:block transition fill-current -ml-0.5' src={RightArr} />
+                   <Icon className='block sm:hidden transition fill-current -ml-0.5' src={DownArr} />
+                 </span>
 
-          <ul in:fly={{ duration: 250, y: 50 }} out:fade={{ duration: 100 }} class='popover bg-white dark:bg-gray-900 rounded-xl shadow-lg w-60 mt-6 ml-4 sm:mt-4 sm:ml-2 flex flex-col divide-y dark:divide-gray-700' slot=popover>
-            <strong class='bg-black sm:bg-gray-800 text-white dark:bg-black rounded-t-xl -mb-px z-50 py-3 px-4'>Load Game</strong>
-            {#each games as game}
-              <li
-                class='px-4 py-2 text-gray-600 dark:text-gray-200 w-full text-sm cursor-pointer dark:hover:text-blue-500 hover:text-blue-400 inline-flex justify-between items-center transition'
-                title='Load game {game.name}'
-                on:click={load(game)}>
-                {game.name}
+                 <ul in:fly={{ duration: 250, y: 50 }} out:fade={{ duration: 100 }} class='popover bg-white dark:bg-gray-900 rounded-xl shadow-lg w-60 mt-6 ml-4 sm:mt-4 sm:ml-2 flex flex-col divide-y dark:divide-gray-700' slot=popover>
+                   <strong class='bg-black sm:bg-gray-800 text-white dark:bg-black rounded-t-xl -mb-px z-50 py-3 px-4'>Load Game</strong>
+                   {#each games as game}
+                     <li
+                       class='px-4 py-2 text-gray-600 dark:text-gray-200 w-full text-sm cursor-pointer dark:hover:text-blue-500 hover:text-blue-400 inline-flex justify-between items-center transition'
+                       title='Load game {game.name}'
+                       on:click={load(game)}>
+                       {game.name}
 
-                <Picture
-                  alt='{game.name} logo'
-                  src=/assets/{game.game}
-                  className='ml-2 w-16'
-                  aspect=192x96
-                  />
-              </li>
-            {/each}
-          </ul>
-        </Popover>
+                       <Picture
+                         alt='{game.name} logo'
+                         src=/assets/{game.game}
+                         className='ml-2 w-16'
+                         aspect=192x96
+                         />
+                     </li>
+                   {/each}
+                 </ul>
+</Popover>
       {/if}
 
   </div>
 
     <span class='inline-flex relative'>
-      <ThemeToggle />
+      <div class=link>
+        <ThemeToggle />
+        <span>Theme</span>
+      </div>
 
       {#each pages as p}
         <a
@@ -113,10 +116,32 @@
 <style>
   .fauxnav { @apply h-12; }
 
+  .p-container > div {
+    @apply hidden
+  }
+  .p-container span {
+    @apply w-full justify-evenly
+  }
+
   nav {
     position: fixed;
+    bottom: 0;
     z-index: 5000;
-    @apply container mx-auto mb-8 md:mb-2 bg-black sm:bg-white text-white sm:text-black;
+    @apply container mx-auto pt-2 md:mb-2 bg-black sm:bg-white text-white sm:text-black;
+  }
+
+  a.link, div.link {
+    @apply flex flex-col items-center justify-center text-center
+  }
+
+  a.link :global(svg) {
+    transform: scale(1.6);
+    @apply mb-2;
+  }
+
+  div.link :global(svg) {
+    transform: scale(1.2);
+    @apply bottom-2;
   }
 
   :global(.dark) nav {
@@ -144,7 +169,7 @@
   h1 { @apply text-base sm:text-xl border-transparent border-b-2 transition -mb-1.5 sm:mb-0; }
 
   a.home { @apply inline-flex items-center pt-4 ml-4 -mt-4 md:mt-0 md:-ml-2 h-12; }
-  a.link { @apply inline-flex items-center gap-x-1 border-black transition p-2 px-3 md:p-4 text-sm md:text-base; }
+  a.link, div.link { @apply inline-flex items-center gap-x-1 border-black transition p-2 px-3 md:p-4 text-sm md:text-base; }
 
   a.link.active { @apply border-b-2 bg-gray-50 text-black cursor-default; }
   a.link:not(.active) { @apply sm:text-gray-500 hover:text-black  cursor-pointer; }
